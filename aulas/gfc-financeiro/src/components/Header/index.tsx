330d78a0cb42d76ad462b47e29087621aa456eaf
ALLAN VERGILIO FELIZZETI <alllanvergilio@gmail.com>
2023-01-08 18:07:08 -0300

    Finalizando Front

--- aulas/gfc-financeiro/src/components/Header/index.tsx
+++ aulas/gfc-financeiro/src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import { HeaderContainer, HeaderContent, NewTrasationButton } from "./styles";
-
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
       <img src={logoImg} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+          <NewTrasationButton>Nova Transação</NewTrasationButton>
+          </Dialog.Trigger>
 
-        <NewTrasationButton>Nova Transação</NewTrasationButton>
+          <NewTransactionModal />
+        </Dialog.Root>
+        
       </HeaderContent>
     </HeaderContainer>
   )
